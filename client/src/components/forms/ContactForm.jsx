47d5ff51c1bd8a95606ac671940ecfc2e1aea7e3
Mel Incognito <melxincognito@gmail.com>
2022-02-09 14:31:06 -0700

    added to contact form

--- client/src/components/forms/ContactForm.jsx
+++ client/src/components/forms/ContactForm.jsx
@@ -1,39 +1,83 @@
-import React from "react";
-import { Card } from "@mui/material";
-import Box from "@mui/material/Box";
-import Input from "@mui/material/Input";
-import InputLabel from "@mui/material/InputLabel";
-import InputAdornment from "@mui/material/InputAdornment";
-import FormControl from "@mui/material/FormControl";
-import TextField from "@mui/material/TextField";
-import AccountCircle from "@mui/icons-material/AccountCircle";
+import * as React from "react";
+import {
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+  MenuItem,
+} from "@mui/material";
+
+// forms global variables
+const servicesOptions = [
+  { value: "Construction", label: "Construction" },
+  { value: "Plumbing", label: "Plumbing" },
+  { value: "Electrical", label: "Electrical" },
+];
 
 export default function ContactForm() {
+  // form functionality variables
+
+  const [serviceInquiry, setServiceInquiry] = React.useState("Construction");
+
+  const handleServiceChange = (event) => {
+    setServiceInquiry(event.target.value);
+  };
+
+  // styles variables
   const cardStyles = {
     padding: 4,
   };
   const textFieldStyles = {
-    margin: "1px 0px",
+    margin: "10px 0px",
   };
 
   return (
     <>
       <Card sx={cardStyles}>
-        {" "}
-        <TextField fullWidth sx={textFieldStyles} label="Name" id="fullWidth" />
-        <TextField
-          fullWidth
-          sx={textFieldStyles}
-          label="Contact Phone"
-          id="fullWidth"
-        />
-        <TextField
-          fullWidth
-          sx={textFieldStyles}
-          rows="3"
-          label="Message"
-          id="fullWidth"
-        />
+        <CardContent>
+          <Typography> Contact Beyond Services </Typography>
+        </CardContent>
+        <CardContent>
+          <TextField
+            fullWidth
+            sx={textFieldStyles}
+            label="Name"
+            id="contactName"
+            required
+          />
+          <TextField
+            fullWidth
+            sx={textFieldStyles}
+            label="Contact Phone"
+            type="tel"
+            id="contactPhone"
+            required
+          />
+          <TextField
+            sx={textFieldStyles}
+            id="serviceInquirySelection"
+            fullWidth
+            select
+            label="Service Catefory"
+            value={serviceInquiry}
+            onChange={handleServiceChange}
+            helperText="Select the service category you're inquiring about"
+          >
+            {servicesOptions.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            fullWidth
+            multiline
+            sx={textFieldStyles}
+            rows={5}
+            label="Message"
+            id="fullWidth"
+          />
+        </CardContent>
       </Card>
     </>
   );
