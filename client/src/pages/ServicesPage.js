4301a66661446dce0e1b093665770cd65f442748
Mel Incognito <melxincognito@gmail.com>
2022-02-16 13:12:27 -0700

    added services laid out

--- client/src/pages/ServicesPage.js
+++ client/src/pages/ServicesPage.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Card, CardContent, Typography } from "@mui/material";
 
 export default function ServicesPage() {
   return (
-    <div>
-      {" "}
-      <h1>Services Page </h1>
-    </div>
+    <>
+      <Card>
+        <CardContent>
+          <Typography variant="h3"> Services</Typography>
+          <Typography>
+            {" "}
+            We are able to provide our services to both commercial and
+            residential customers!
+          </Typography>
+          <Typography>
+            {" "}
+            a track homes, home additions, home renovations. We can also do
+            whole homes, including custom luxury homes.{" "}
+          </Typography>
+          <Typography> We are also able to offer plumbing services</Typography>
+        </CardContent>
+      </Card>
+    </>
   );
 }
