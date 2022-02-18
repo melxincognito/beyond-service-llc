0517a89367c1b223e3370cd98922284c1ff3b6bc
Mel Incognito <melxincognito@gmail.com>
2022-02-17 18:17:11 -0700

    added theme

---
+++ client/src/assets/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff9800",
+    },
+    secondary: {
+      main: "#00e5ff",
+    },
+  },
+});
+
+export default theme;
