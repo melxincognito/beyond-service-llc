348434d222495b751122e82385f89a3750b5c1fc
Mel Incognito <melxincognito@gmail.com>
2022-03-16 19:29:19 -0700

    added phone number formatter

--- client/src/components/forms/ContactForm.jsx
+++ client/src/components/forms/ContactForm.jsx
@@ -36,13 +36,44 @@ const servicesOptions = [
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
+function formatPhoneNumber(value) {
+  // if input value is falsy eg if the user deletes the input, then just return
+  if (!value) return value;
+
+  // clean the input for any non-digit values.
+  const phoneNumber = value.replace(/[^\d]/g, "");
+
+  // phoneNumberLength is used to know when to apply our formatting for the phone number
+  const phoneNumberLength = phoneNumber.length;
+
+  // we need to return the value with no formatting if its less then four digits
+  // this is to avoid weird behavior that occurs if you  format the area code to early
+  if (phoneNumberLength < 4) return phoneNumber;
+
+  // if phoneNumberLength is greater than 4 and less the 7 we start to return
+  // the formatted number
+  if (phoneNumberLength < 7) {
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+  }
+
+  // finally, if the phoneNumberLength is greater then seven, we add the last
+  // bit of formatting and return it.
+  return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+    3,
+    6
+  )}-${phoneNumber.slice(6, 10)}`;
+}
 
 export default function ContactForm() {
   // form functionality variables
 
+  // state to set tthe popup box once form is submitted to open or close
   const [open, setOpen] = React.useState(false);
 
   const [serviceInquiry, setServiceInquiry] = React.useState("Construction");
+
+  // state for formatting phone number input value
+  const [inputValue, setInputValue] = React.useState("");
 
   const handleServiceChange = (event) => {
     setServiceInquiry(event.target.value);
@@ -56,6 +87,13 @@ export default function ContactForm() {
   const handleClose = () => {
     setOpen(false);
     routeChange();
+  };
+  // handling phone number input to format the number for better visibility
+  const handleInput = (e) => {
+    // this is where we'll call the phoneNumberFormatter function
+    const formattedPhoneNumber = formatPhoneNumber(e.target.value);
+    // we'll set the input value using our setInputValue
+    setInputValue(formattedPhoneNumber);
   };
 
   // form forwarding functions
@@ -140,6 +178,8 @@ export default function ContactForm() {
                 type="tel"
                 id="contactPhone"
                 name="phone"
+                onChange={(e) => handleInput(e)}
+                value={inputValue}
                 required
               />
             </div>
