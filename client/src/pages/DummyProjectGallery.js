f49fe1e6a64893abe10e8cf4a9e16e67a5c33116
Mel Incognito <melxincognito@gmail.com>
2022-03-31 12:04:13 -0700

    getting single image from firebase storage on screen

--- client/src/pages/DummyProjectGallery.js
+++ client/src/pages/DummyProjectGallery.js
@@ -1,22 +1,49 @@
-import React from "react";
+import * as React from "react";
 import { storage } from "../firebase-config";
-import { ref, listAll } from "firebase/storage";
+import { ref, getDownloadURL } from "firebase/storage";
 
-const listRef = ref(storage, "images/");
-
-listAll(listRef)
-  .then((res) => {
-    res.prefixes.forEach((folderRef) => {
-      console.log(folderRef);
-    });
-    res.items.forEach((itemRef) => {
-      console.log(itemRef);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const listRef = ref(storage, "images/79d0748c-5600-479b-8249-733bfc99a44c.JPG");
 
 export default function DummyProjectGallery() {
-  return <div></div>;
+  const [allImages, setImages] = React.useState([]);
+
+  function getFromFirebase() {
+    let storageRef = storage.ref();
+
+    storageRef
+      .listAll()
+      .then(function (res) {
+        res.items.forEach((imageRef) => {
+          imageRef.ref.openStream().then((url) => {
+            setImages((allImages) => [...allImages, url]);
+          });
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  function dothis() {
+    getDownloadURL(listRef).then((url) => {
+      console.log(url);
+      setImages([url]);
+      console.warn(allImages);
+      console.log(allImages.length);
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={dothis}> Click me </button>
+      <div id="body">
+        {allImages.map((image, index) => {
+          return (
+            <div key={index}>
+              <img src={image} width="130px" height="130px" alt="" />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
