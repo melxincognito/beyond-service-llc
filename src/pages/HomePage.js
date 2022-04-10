f7735665b9fd5247e44975ab33c48864edfe1fcd
Mel Incognito <melxincognito@gmail.com>
2022-04-10 10:45:47 -0700

    hid content for mobile display

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { Card, CardContent, Typography, Box, Container } from "@mui/material";
 import HeaderImg from "../assets/photos/sunset.jpg";
 import homepageImgGalleryData from "../data/homepageImageGallery.json";
@@ -11,6 +11,47 @@ import "../App.css";
 import BottomNavLabel from "../components/navigation/BottomNavLabel";
 
 export default function HomePage() {
+  const John = {
+    name: "John",
+    review:
+      "I love how Jonathan and Juan really took the time to make sure they were making my home look exactly the way I wanted. Everything turned out phenomenal.",
+    img: "https://live.staticflickr.com/65535/51995160128_ef797fb6dd_z.jpg",
+  };
+
+  const Stacy = {
+    name: "Stacy",
+    review:
+      "Jonathan and Juan are amazing! They constructed the PERFECT luxury bathroom for me. I'd recommend them to anyone in the valley!",
+    img: "https://live.staticflickr.com/65535/51995653115_8d5de6ed7f_z.jpg",
+  };
+
+  const Robin = {
+    name: "Robin",
+    review:
+      "My Kitchen remodel turned out amazing! I couldn't be happier with the results, they really went above and beyond to make everything look so great!",
+    img: "https://live.staticflickr.com/65535/51995387404_bd22ae445c_z.jpg",
+  };
+
+  const [name, setName] = React.useState(John.name);
+  const [review, setReview] = React.useState(John.review);
+  const [custImg, setCustImg] = React.useState(John.img);
+
+  const flipReviews = () => {
+    if (name === "Stacy") {
+      setName(John.name);
+      setReview(John.review);
+      setCustImg(John.img);
+    } else if (name === "John") {
+      setName(Robin.name);
+      setReview(Robin.review);
+      setCustImg(Robin.img);
+    } else {
+      setName(Stacy.name);
+      setReview(Stacy.review);
+      setCustImg(Stacy.img);
+    }
+  };
+
   // center card style
   const cardStyles = {
     borderRadius: "5px 25px 5px 25px",
@@ -26,24 +67,14 @@ export default function HomePage() {
     spacing: 12,
   };
 
-  const contentContainerStyles = {
-    display: "grid",
-    justifyItems: "center",
-    alignContent: "center",
-    justifyContent: "center",
-    gridTemplateColumns: "1fr",
-    gridTemplateRows: "repeat(1, 1fr)",
-    gridRowGap: "5px",
-  };
-
   const topImageContainerStyles = {
-    display: "flex",
     alignContent: "center",
     justifyContent: "center",
     alignItems: "center",
     justifyItems: "center",
     width: "100%",
     marginTop: "12rem",
+    display: { xs: "none", md: "flex" },
   };
 
   const topImageStyles = {
@@ -66,7 +97,6 @@ export default function HomePage() {
   };
   const introContentCardStyles = {
     borderRadius: "5px 25px 5px 25px",
-    display: "grid",
     alignContent: "center",
     justifyContent: "center",
     textAlign: "center",
@@ -74,6 +104,7 @@ export default function HomePage() {
     margin: 5,
     padding: 2,
     backgroundColor: "rgba(255, 255, 255, 0.53)",
+    display: { xs: "grid", md: "grid" },
   };
 
   const customerReviewContainerStyles = {
@@ -82,6 +113,8 @@ export default function HomePage() {
     borderRadius: 1.5,
     display: "flex",
     justifyContent: "space-around",
+    alignContent: "center",
+    justifyItems: "center",
     alignItems: "center",
     boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.5)",
     marginBottom: 10,
@@ -95,6 +128,7 @@ export default function HomePage() {
     boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.4)",
     margin: "10px auto 10px auto",
     backgroundColor: "rgba(255, 255, 255, 0.53)",
+    minWidth: "100%",
   };
 
   const horizontalImgGalleryContainerStyles = {
@@ -103,6 +137,7 @@ export default function HomePage() {
     bgcolor: "accordion.main",
     left: 0,
     right: 0,
+    display: { xs: "none", md: "grid" },
   };
   const spacerStyles = {
     margin: "0 0 -50px 0",
@@ -118,7 +153,10 @@ export default function HomePage() {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
       >
-        <Box id="headImgContainer" sx={contentContainerStyles}>
+        <Box
+          id="desktopViewHomepage"
+          sx={{ display: { xs: "none", md: "grid" } }}
+        >
           <Box id="imageContainer" sx={topImageContainerStyles}>
             <img
               style={topImageStyles}
@@ -146,149 +184,168 @@ export default function HomePage() {
               </Card>
             </Box>
           </Box>
-        </Box>{" "}
-        <Box
-          id="pageContentContainer"
-          sx={{
-            marginTop: "20rem",
-            display: "grid",
-            gridTemplateColumns: "auto",
-          }}
-        >
-          <Box id="introContentContainer" sx={introCardContentContainerStyles}>
-            <div className="hexagonTop" id="hexagonTop"></div>
-            <Box>
-              <Card sx={introContentCardStyles}>
-                <CardContent
-                  sx={{
-                    borderRadius: "5px 25px 5px 25px",
-                    boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.5)",
-                    margin: 1,
-                    backgroundColor: "rgba(255, 255, 255, 0.53)",
-                  }}
-                >
-                  <Typography variant="h4" color="secondary.main">
-                    {" "}
-                    Who are we?
-                  </Typography>
-                </CardContent>{" "}
-                <CardContent>
-                  {" "}
-                  <Typography>
-                    We're a team of Phoenix locals providing only the BEST
-                    general contracting services in the valley. We provide all
-                    types of services to our customers. From home remodels,
-                    plumbing to HVAC. Find out for yourself why we remain as one
-                    of Phoenix's preferred General Contractors. Look around our
-                    site to find out more about what Beyond Services has to
-                    offer. Feel free to use our contact form or give us a call
-                    if you want us to help turn your house into the perfect
-                    home!
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Box>
-            <Box sx={{ display: { xs: "none", md: "flex" } }}>
-              <Box sx={{ position: "relative" }}>
-                <Card
-                  sx={{
-                    borderRadius: "50%",
-                    width: "260px",
-                    height: "260px",
-                    position: "relative",
-                    top: "12rem",
-                    left: "2rem",
-                    border: "5px #801313 solid;",
-                    boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.6)",
-                  }}
-                >
-                  <img
-                    src="https://images.unsplash.com/photo-1599809275671-b5942cabc7a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-                    alt="pool"
-                    style={{
-                      width: "260px",
-                      height: "260px",
-                      borderRadius: "50%",
-                    }}
-                  />
-                </Card>
-                <Card
-                  sx={{
-                    borderRadius: "50%",
-                    width: "350px",
-                    height: "350px",
-                    position: "relative",
-                    bottom: "15rem",
-                    left: "9rem",
-                    zIndex: -1,
-                    border: "5px #000000 solid;",
-                    boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.6)",
-                  }}
-                >
-                  {" "}
-                  <img
-                    src="https://images.unsplash.com/flagged/photo-1573168710465-7f7da9a23a15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVtb2RlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-                    alt="pool"
-                    style={{
-                      width: "350px",
-                      height: "350px",
-                      borderRadius: "50%",
-                    }}
-                  />
-                </Card>
-              </Box>
-            </Box>{" "}
-            <div className="hexagonBottom" id="hexagonBottom"></div>
-          </Box>
-          <Box id="spacer" sx={spacerStyles}>
-            {" "}
-          </Box>
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <Box sx={customerReviewContainerStyles}>
-              {" "}
-              <ArrowBackIosIcon />
+          <Box
+            id="pageContentContainer"
+            sx={{
+              marginTop: "20rem",
+              display: "grid",
+              gridTemplateColumns: "auto",
+            }}
+          >
+            <Box
+              id="introContentContainer"
+              sx={introCardContentContainerStyles}
+            >
+              <div className="hexagonTop" id="hexagonTop"></div>
               <Box>
-                {" "}
-                <Card sx={customerReviewCardStyles}>
-                  {" "}
-                  <CardContent>
-                    <Container
-                      sx={{
-                        margin: "5px auto",
-                      }}
-                    >
+                <Card sx={introContentCardStyles}>
+                  <CardContent
+                    sx={{
+                      borderRadius: "5px 25px 5px 25px",
+                      boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.5)",
+                      margin: 1,
+                      backgroundColor: "rgba(255, 255, 255, 0.53)",
+                    }}
+                  >
+                    <Typography variant="h4" color="secondary.main">
                       {" "}
-                      <img
-                        src="https://images.unsplash.com/photo-1642792735536-b6f7ef18b918?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80"
-                        alt="person"
-                        style={{
-                          width: "115px",
-                          height: "110px",
-                          borderRadius: "50%",
-                          boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.6)",
-                        }}
-                      />
-                    </Container>{" "}
-                    <Typography> John</Typography>
-                    <Typography variant="caption">
-                      {" "}
-                      "Juan and Jonathan did a great job remodeling my home. I
-                      would recommend them to anyone in the valley"
+                      Who are we?
                     </Typography>
-                    <div>
-                      <ul style={{ display: "inline-flex" }}>
-                        <li style={{ marginRight: 20 }}> </li>
-                        <li style={{ marginRight: 20 }}> </li>
-                        <li> </li>
-                      </ul>
-                    </div>
+                  </CardContent>{" "}
+                  <CardContent>
+                    {" "}
+                    <Typography>
+                      We're a team of Phoenix locals providing only the BEST
+                      general contracting services in the valley. We provide all
+                      types of services to our customers. From home remodels,
+                      plumbing to HVAC. Find out for yourself why we remain as
+                      one of Phoenix's preferred General Contractors. Look
+                      around our site to find out more about what Beyond
+                      Services has to offer. Feel free to use our contact form
+                      or give us a call if you want us to help turn your house
+                      into the perfect home!
+                    </Typography>
                   </CardContent>
                 </Card>
               </Box>
-              <ArrowForwardIosIcon />
+              <Box>
+                <Box sx={{ position: "relative" }}>
+                  <Card
+                    sx={{
+                      borderRadius: "50%",
+                      width: "260px",
+                      height: "260px",
+                      position: "relative",
+                      top: "12rem",
+                      left: "2rem",
+                      border: "5px #801313 solid;",
+                      boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.6)",
+                    }}
+                  >
+                    <img
+                      src="https://images.unsplash.com/photo-1599809275671-b5942cabc7a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+                      alt="pool"
+                      style={{
+                        width: "260px",
+                        height: "260px",
+                        borderRadius: "50%",
+                      }}
+                    />
+                  </Card>
+                  <Card
+                    sx={{
+                      borderRadius: "50%",
+                      width: "350px",
+                      height: "350px",
+                      position: "relative",
+                      bottom: "15rem",
+                      left: "9rem",
+                      zIndex: -1,
+                      border: "5px #000000 solid;",
+                      boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.6)",
+                    }}
+                  >
+                    {" "}
+                    <img
+                      src="https://images.unsplash.com/flagged/photo-1573168710465-7f7da9a23a15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVtb2RlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+                      alt="pool"
+                      style={{
+                        width: "350px",
+                        height: "350px",
+                        borderRadius: "50%",
+                      }}
+                    />
+                  </Card>
+                </Box>
+              </Box>{" "}
+              <div className="hexagonBottom" id="hexagonBottom"></div>
             </Box>
-          </Box>
-        </Box>{" "}
+            <Box id="spacer" sx={spacerStyles}>
+              {" "}
+            </Box>
+            <Box
+              sx={{
+                display: { xs: "none", md: "flex" },
+                justifyContent: "center",
+              }}
+            >
+              <Box sx={customerReviewContainerStyles}>
+                {" "}
+                <ArrowBackIosIcon onClick={flipReviews} />
+                <Box>
+                  {" "}
+                  <Card sx={customerReviewCardStyles}>
+                    {" "}
+                    <CardContent>
+                      <Container
+                        sx={{
+                          margin: "5px auto",
+                          minWidth: "100%",
+                        }}
+                      >
+                        {" "}
+                        <img
+                          src={`${custImg}`}
+                          alt={`${name}`}
+                          style={{
+                            width: "115px",
+                            height: "115px",
+                            borderRadius: "50%",
+                            boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.6)",
+                          }}
+                        />
+                      </Container>{" "}
+                      <Container sx={{ width: "80%" }}>
+                        <Typography> {name}</Typography>
+                        <Typography variant="caption"> {review}</Typography>
+                      </Container>
+                      <div>
+                        <ul style={{ display: "inline-flex" }}>
+                          <li
+                            style={{
+                              marginRight: 20,
+                            }}
+                          >
+                            {" "}
+                          </li>
+                          <li
+                            style={{
+                              marginRight: 20,
+                            }}
+                          >
+                            {" "}
+                          </li>
+                          <li> </li>
+                        </ul>
+                      </div>{" "}
+                    </CardContent>
+                  </Card>
+                </Box>
+                <ArrowForwardIosIcon onClick={flipReviews} />
+              </Box>
+            </Box>
+          </Box>{" "}
+        </Box>
         {/*horizontal image gallery in seperate container on bottom bc it keeps getting pushed to the top */}
         <Box
           id="horizontalImgGalleryContainer"
