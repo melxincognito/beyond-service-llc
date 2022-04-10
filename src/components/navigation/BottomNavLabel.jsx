ab46fe213a9f343f74e4f59a1b6be458b5b8a531
Mel Incognito <melxincognito@gmail.com>
2022-04-09 22:14:49 -0700

    changed colors

--- src/components/navigation/BottomNavLabel.jsx
+++ src/components/navigation/BottomNavLabel.jsx
@@ -6,12 +6,12 @@ export default function BottomNavLabel() {
     <div>
       <Paper
         sx={{
-          bgcolor: "primary.main",
+          bgcolor: "secondary.main",
           position: "fixed",
           bottom: 0,
           left: 0,
           right: 0,
-          color: "secondary.main",
+          color: "white",
           display: "flex",
           alignContent: "center",
           justifyContent: "center",
