9e66675edab8083c6ed350b1d34019005a456012
Mel Incognito <melxincognito@gmail.com>
2022-08-04 09:10:38 -0500

    refactored code

--- src/components/reviews/ApprovedClientReviewCard.jsx
+++ src/components/reviews/ApprovedClientReviewCard.jsx
@@ -1,9 +1,9 @@
-import * as React from "react";
+import React from "react";
+import { db } from "../../firebase-config";
+import { set, ref, remove } from "firebase/database";
 import { Box, Button, Paper, Typography } from "@mui/material";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
-import { db } from "../../firebase-config";
-import { set, ref, remove } from "firebase/database";
 
 import "./TestimonialStyles.css";
 
@@ -45,6 +45,18 @@ export default function ApprovedClientReviewCard(props) {
 
   // styles variables
 
+  const approvedReviewCardStyles = {
+    display: "grid",
+    justifyContent: "center",
+    justifyItems: "start",
+    width: "100%",
+    backgroundColor: "white",
+    gap: "1rem",
+    padding: "1rem",
+    borderRadius: 2,
+    boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.3)",
+  };
+
   const paperStyles = {
     padding: 1,
     display: "flex",
@@ -71,19 +83,7 @@ export default function ApprovedClientReviewCard(props) {
   };
 
   return (
-    <Box
-      sx={{
-        display: "grid",
-        justifyContent: "center",
-        justifyItems: "start",
-        width: "100%",
-        backgroundColor: "white",
-        gap: "1rem",
-        padding: "1rem",
-        borderRadius: 2,
-        boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.3)",
-      }}
-    >
+    <Box sx={approvedReviewCardStyles}>
       <div id="reviewNamefield" style={{ display: "flex", gap: "1rem" }}>
         <Paper elevation={3} sx={paperStyles}>
           <Typography>Client Name: </Typography>
--- src/components/reviews/adminDashboard/CurrentUserHeader.js
+++ src/components/reviews/adminDashboard/CurrentUserHeader.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 
 import { auth } from "../../../firebase-config";
@@ -7,28 +7,27 @@ import { Paper, Typography } from "@mui/material";
 import "../TestimonialStyles.css";
 
 export default function CurrentUserHeader() {
-  const [user, setUser] = React.useState({});
+  const [user, setUser] = useState({});
 
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
   });
 
+  const currentUserPaperStyles = {
+    display: "flex",
+    margin: 0,
+    padding: "0 7px",
+    backgroundColor: "pink",
+    height: "4%",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    bgcolor: "#262121",
+    color: "white",
+    boxShadow: "5px 5px 15px 5px rgba(0,0,0,0.33)",
+  };
+
   return (
-    <Paper
-      id="currentUser"
-      sx={{
-        display: "flex",
-        margin: 0,
-        padding: "0 7px",
-        backgroundColor: "pink",
-        height: "4%",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        bgcolor: "#262121",
-        color: "white",
-        boxShadow: "5px 5px 15px 5px rgba(0,0,0,0.33)",
-      }}
-    >
+    <Paper id="currentUser" sx={currentUserPaperStyles}>
       <Typography> Hello, {user?.displayName}!</Typography>
     </Paper>
   );
